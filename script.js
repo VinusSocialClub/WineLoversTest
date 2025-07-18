5ea42ffa630721cac6ef6ba0701eb9236407b32f
Vinus Social Club <vinussocialclub@gmail.com>
2025-07-18 03:06:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -184,3 +184,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const loginBtn = document.getElementById('loginBtn');
+  if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+      window.location.href = 'login.html';
+    });
+  }
+});
