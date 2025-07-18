869d36bd3d9d5775176f4ab64a5d2e7e295c98a8
Vinus Social Club <vinussocialclub@gmail.com>
2025-07-18 02:35:56 +0100

    Update user.js

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true }, // Nome visível
+  username: { type: String, required: true, unique: true }, // Nome visível no site
   email: { type: String, required: true, unique: true },    // Para login
-  password: { type: String, required: true }                 // Password simples (neste caso)
+  password: { type: String, required: true }
 });
 
 module.exports = model('User', UserSchema);
